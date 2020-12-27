12ac820f41f44bce58fae6e36ce0e346c549b56b
Prachika <prachikapal@gmail.com>
2020-12-27 17:15:14 +0530

    setup static files

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const port = 8000;
 
 const app = express();
 
+app.use(express.static("./assets"));
 app.use(expressLayouts);
 app.use("/", require("./routes"));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+//extract styles and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.listen(port, function (err) {
   if (err) {
