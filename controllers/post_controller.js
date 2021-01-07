eb59b77bbcf6de281224654f8e62224e8a63868b
Prachika <prachikapal@gmail.com>
2021-01-07 13:47:13 +0530

    delete posts and comments

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = function(req,res){
     Post.create({
@@ -12,3 +13,26 @@ module.exports.create = function(req,res){
         return res.redirect("back");
     })
 }
+
+module.exports.destroy = function(req,res) {
+    Post.findById(req.params.id, function(err,post){
+        if(err){
+            console.log("Error in finding the post");
+            return;
+        }
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post : req.params.id},function(err){
+                if(err){
+                    console.log("Error in deleting the Comments");
+                    return;
+                }
+                return res.redirect("/");
+            });
+        }
+        else{
+            return res.redirect("back");
+        }
+    })
+}
+
